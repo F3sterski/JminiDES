e1f41d41a0a61fed8b819ec5781db39575a7ecfa
F3sterski <adam.wojciech.zielke@gmail.com>
2015-03-30 14:03:15 +0200

    Nie mam pojęcia co robię

--- src/pack/JminiDES.java
+++ src/pack/JminiDES.java
@@ -1,10 +1,26 @@
 package pack;
 
 public class JminiDES {
+	
+	 public static final int P8[] = {1,2,4,3,4,3,5,6};
+	// public static final int S0[][] = {{1,0,1},{0,1,0},{};
+	 public static final int S1[][] = {{ 0, 1, 2, 3},{ 2, 0, 1, 3},{ 3, 0, 1,
+         2},{ 2, 1, 0, 3}};
 
+	public static int permutacje( int x, int p[], int pmax){
+      int y = 0;
+      for( int i = 0; i < p.length; ++i){
+         
+    	 y <<= 1;
+         y |= (x >> (pmax - p[i])) & 1;
+     }
+      return y;
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x=1401;
+		System.out.println(x+" "+permutacje(x,P8,9));
+		
 	}
 
 }
